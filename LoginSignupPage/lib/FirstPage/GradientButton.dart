1e5b70cf3941aa8a35bf9d96727df084c8555f92
MohammadPrHosseini <mr.mohamad.hoseini05@gmail.com>
2024-06-04 09:28:05 +0330

    improve buttons

--- LoginSignupPage/lib/FirstPage/GradientButton.dart
+++ LoginSignupPage/lib/FirstPage/GradientButton.dart
@@ -1,17 +1,42 @@
+import 'dart:ui';
+
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:login_signup_page/FirstPage/Pallete.dart';
+
 class GradientButton extends StatelessWidget {
   const GradientButton({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return ElevatedButton(onPressed: (){},
-    style: ElevatedButton.styleFrom(
-      fixedSize: const Size(200, 20)
-    ),
-    child:const Text('ورود',style: TextStyle(
-      color: Pallete.whiteColor
-    ),),
+    return Container(
+        decoration:  BoxDecoration(
+          gradient: const LinearGradient(colors: [
+            Pallete.gradient1,
+            Pallete.gradient2,
+            Pallete.gradient3,
+          ],
+            begin: Alignment.bottomLeft
+          ),
+
+          borderRadius: BorderRadius.circular(10)
+        ),
+        child: ElevatedButton(
+          onPressed: () {},
+          style: ElevatedButton.styleFrom(
+              fixedSize: const Size(400, 60),
+              backgroundColor:Colors.transparent,
+          ),
+          child: const Text(
+            'ورود',
+            style: TextStyle(
+              color: Pallete.whiteColor,
+              fontSize: 17,
+              fontWeight: FontWeight.w600,
+            ),
+          ),
+
+        ),
     );
   }
 }
--- LoginSignupPage/lib/FirstPage/Signup.dart
+++ LoginSignupPage/lib/FirstPage/Signup.dart
@@ -1,3 +1,5 @@
+import 'dart:ui';
+
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:login_signup_page/FirstPage/GradientButton.dart';
@@ -5,7 +7,8 @@ import 'package:login_signup_page/FirstPage/Pallete.dart';
 import 'package:login_signup_page/FirstPage/login_screen.dart';
 
 class Signup extends StatelessWidget {
-  const Signup({super.key});
+  final String situation;
+  const Signup({super.key, required this.situation});
 
   @override
   Widget build(BuildContext context) {
@@ -56,45 +59,65 @@ class Signup extends StatelessWidget {
             const SizedBox(
               height: 10,
             ),
-            TextFormField(
-              obscureText: true,
-              decoration: const InputDecoration(
-                hintText: 'Confirm Password',
-                contentPadding: EdgeInsets.all(20),
-                enabledBorder: OutlineInputBorder(
-                  borderSide: BorderSide(
-                    color: Pallete.borderColor,
+            if(situation=='Signup')
+              TextFormField(
+                obscureText: true,
+                decoration: const InputDecoration(
+                  hintText: 'Confirm Password',
+                  contentPadding: EdgeInsets.all(20),
+                  enabledBorder: OutlineInputBorder(
+                    borderSide: BorderSide(
+                      color: Pallete.borderColor,
+                    ),
                   ),
-                ),
-                focusedBorder: OutlineInputBorder(
-                  borderSide: BorderSide(
-                    color: Pallete.gradient2,
-                    width: 4,
+                  focusedBorder: OutlineInputBorder(
+                    borderSide: BorderSide(
+                      color: Pallete.gradient2,
+                      width: 4,
+                    ),
                   ),
                 ),
               ),
-            ),
+
             const SizedBox(
-              height: 20,
+              height: 220,
             ),
             const GradientButton(),
             const SizedBox(
               height: 10,
             ),
-            ElevatedButton(
-              onPressed: () {
-                Navigator.push(context,
-                MaterialPageRoute(builder: (context)=>const LoginScreen()));
-              },
-              style: ElevatedButton.styleFrom(fixedSize: const Size(200, 20)),
-              child: const Text(
-                'بازگشت',
-                style: TextStyle(
-                  color: Pallete.whiteColor,
+            Container(
+              decoration: const BoxDecoration(
+                gradient: LinearGradient(
+                  colors: [
+                    Pallete.gradient1,
+                    Pallete.gradient2,
+                    Pallete.gradient3
+                  ]
+                )
+              ),
+              child: ElevatedButton(
+                onPressed: () {
+                  Navigator.push(context,
+                      MaterialPageRoute(builder: (context)=>const LoginScreen()));
+                },
+
+                style: ElevatedButton.styleFrom(
+                    fixedSize: const Size(400, 60),
+                    backgroundColor: Colors.transparent),
+                child: const Text(
+                  'بازگشت',
+                  style: TextStyle(
+                    color: Pallete.whiteColor,
+                    fontSize: 17,
+                    fontWeight: FontWeight.w600,
+                  ),
                 ),
               ),
-            ),
+            )
+
           ],
-        ));
+        )
+    );
   }
 }
