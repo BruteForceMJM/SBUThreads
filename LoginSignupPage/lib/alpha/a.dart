ac352dd8cb3e0d4c024554f9e885d041c1dc46e7
MohammadPrHosseini <mr.mohamad.hoseini05@gmail.com>
2024-06-20 23:52:26 +0330

    delete useless dartfiles

--- LoginSignupPage/lib/alpha/a.dart
+++ LoginSignupPage/lib/alpha/a.dart
@@ -8,25 +8,106 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Scaffold(
-        body: Container(
-          color: Colors.black,
-          child: Column(
-            children: [
-              Expanded(
-                child: Container(
-                  color: Colors.black,
-                ),
-              ),
-              Container(
-                height: 100,
-                color: Colors.red,
+      title: 'Flutter Dialog Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: HomePage(),
+    );
+  }
+}
 
-              ),
-            ],
-          ),
+class HomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Dialog Demo'),
+      ),
+      body: Center(
+        child: ElevatedButton(
+          onPressed: () {
+            showDialog(
+              context: context,
+              builder: (BuildContext context) {
+                return UserInputDialog();
+              },
+            );
+          },
+          child: Text('Show Dialog'),
         ),
       ),
     );
   }
 }
+
+class UserInputDialog extends StatefulWidget {
+  @override
+  _UserInputDialogState createState() => _UserInputDialogState();
+}
+
+class _UserInputDialogState extends State<UserInputDialog> {
+  final _formKey = GlobalKey<FormState>();
+  String _username = '';
+  String _password = '';
+
+  @override
+  Widget build(BuildContext context) {
+    return AlertDialog(
+      title: Text('Enter Credentials'),
+      content: Form(
+        key: _formKey,
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            TextFormField(
+              decoration: InputDecoration(labelText: 'Username'),
+              validator: (value) {
+                if (value == null || value.isEmpty) {
+                  return 'Please enter your username';
+                }
+                return null;
+              },
+              onSaved: (value) {
+                _username = value!;
+              },
+            ),
+            TextFormField(
+              decoration: InputDecoration(labelText: 'Password'),
+              obscureText: true,
+              validator: (value) {
+                if (value == null || value.isEmpty) {
+                  return 'Please enter your password';
+                }
+                return null;
+              },
+              onSaved: (value) {
+                _password = value!;
+              },
+            ),
+          ],
+        ),
+      ),
+      actions: [
+        TextButton(
+          onPressed: () {
+            Navigator.of(context).pop();
+          },
+          child: Text('Cancel'),
+        ),
+        ElevatedButton(
+          onPressed: () {
+            if (_formKey.currentState!.validate()) {
+              _formKey.currentState!.save();
+              print('Username: $_username');
+              print('Password: $_password');
+              Navigator.of(context).pop();
+            }
+          },
+          child: Text('Save'),
+        ),
+      ],
+    );
+  }
+}
+
