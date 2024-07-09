6114c808e809cec8debae62e72f2e3cdfd00757b
MohammadPrHosseini <mr.mohamad.hoseini05@gmail.com>
2024-07-09 08:47:03 +0330

    improve news section and handle render error

--- LoginSignupPage/lib/FirstPage/second_edition_student_news_page.dart
+++ LoginSignupPage/lib/FirstPage/second_edition_student_news_page.dart
@@ -8,10 +8,11 @@ import 'package:login_signup_page/FirstPage/second_edition_student_home_page.dar
 import 'package:login_signup_page/FirstPage/second_edition_student_work_space.dart';
 import 'package:login_signup_page/FirstPage/student_news_page.dart';
 
-void main(){
+void main() {
   runApp(const Qwerty());
 }
-class Qwerty extends StatelessWidget{
+
+class Qwerty extends StatelessWidget {
   const Qwerty({super.key});
 
   @override
@@ -21,7 +22,6 @@ class Qwerty extends StatelessWidget{
       home: StudentNewsPageSection2(),
     );
   }
-
 }
 
 class StudentNewsPageSection2 extends StatefulWidget {
@@ -33,146 +33,163 @@ class StudentNewsPageSection2 extends StatefulWidget {
 
 class _StudentNewsPageSection2State extends State<StudentNewsPageSection2> {
   InAppWebViewController? webViewController;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Pallete.backgroundColor,
-      body: Container(
-        color: Pallete.backgroundColor,
-        child: Column(
-          children: [
-            const SizedBox(height: 800,),
-            Container(
-              color: Colors.blueAccent,
-              height: 83,
-              child: Column(
-                children: [
-                  Row(
-                    children: [
-                      const SizedBox(
-                        height: 20,
-                        width: 20,
-                      ),
-                      IconButton(
-                          onPressed: () {
-                            Navigator.pushReplacement(
-                                context,
-                                MaterialPageRoute(
-                                    builder: (context) =>
-                                    const StudentHomePageEdit()));
-                          },
-                          icon: const Icon(
-                            CupertinoIcons.house_fill,
-                            color: Colors.black,
-                            size: 30,
-                          )),
-                      const SizedBox(
-                        width: 20,
-                        height: 0,
-                      ),
-                      IconButton(
-                        onPressed: (){
-
+      body: Column(
+        children: [
+          Expanded(
+            child: InAppWebView(
+              initialUrlRequest: URLRequest(url: Uri.parse('https://news.sbu.ac.ir/')),
+              onWebViewCreated: (controller) {
+                webViewController = controller;
+              },
+              onLoadStart: (controller, url) {
+                print("Started loading: $url");
+              },
+              onLoadStop: (controller, url) async {
+                print("Stopped loading: $url");
+              },
+              onLoadError: (controller, url, code, message) {
+                print("Error loading: $url, Code: $code, Message: $message");
+              },
+              onProgressChanged: (controller, progress) {
+                if (progress == 100) {
+                  print("Page loaded");
+                }
+              },
+            ),
+          ),
+          Container(
+            color: Colors.blueAccent,
+            height: 83,
+            child: Column(
+              children: [
+                Row(
+                  children: [
+                    const SizedBox(
+                      height: 20,
+                      width: 20,
+                    ),
+                    IconButton(
+                        onPressed: () {
+                          Navigator.pushReplacement(
+                              context,
+                              MaterialPageRoute(
+                                  builder: (context) =>
+                                  const StudentHomePageEdit()));
                         },
                         icon: const Icon(
-                          Icons.account_circle,
+                          CupertinoIcons.house_fill,
                           color: Colors.black,
                           size: 30,
-                        ),
+                        )),
+                    const SizedBox(
+                      width: 20,
+                      height: 0,
+                    ),
+                    IconButton(
+                      onPressed: () {
+
+                      },
+                      icon: const Icon(
+                        Icons.account_circle,
+                        color: Colors.black,
+                        size: 30,
                       ),
-                      const SizedBox(width: 20,),
-                      IconButton(
-                          onPressed: () {
-                            Navigator.pushReplacement(
-                                context,
-                                MaterialPageRoute(
-                                    builder: (context) =>
-                                        StudentWorkPageEdit()));
-                          },
-                          icon: const Icon(
-                            Icons.task_rounded,
-                            color: Colors.black,
-                            size: 30,
-                          )),
-                      const SizedBox(
-                        width: 20,
+                    ),
+                    const SizedBox(width: 20,),
+                    IconButton(
+                        onPressed: () {
+                          Navigator.pushReplacement(
+                              context,
+                              MaterialPageRoute(
+                                  builder: (context) =>
+                                      StudentWorkPageEdit()));
+                        },
+                        icon: const Icon(
+                          Icons.task_rounded,
+                          color: Colors.black,
+                          size: 30,
+                        )),
+                    const SizedBox(
+                      width: 20,
+                    ),
+                    IconButton(
+                      onPressed: () {},
+                      icon: const Icon(
+                        Icons.add_chart_rounded,
+                        color: Colors.black,
+                        size: 30,
                       ),
-                      IconButton(
+                    ),
+                    const SizedBox(
+                      width: 20,
+                    ),
+                    IconButton(
+                        onPressed: () {
+                          Navigator.pushReplacement(context,
+                              MaterialPageRoute(builder: (context) => const StudentNewsPageSection2())
+                          );
+                        },
+                        icon: const Icon(
+                          CupertinoIcons.news_solid,
+                          color: Colors.white,
+                          size: 30,
+                        )),
+                    const SizedBox(
+                      width: 20,
+                    ),
+                    IconButton(
                         onPressed: () {},
                         icon: const Icon(
-                          Icons.add_chart_rounded,
+                          Icons.add_business,
                           color: Colors.black,
                           size: 30,
-                        ),
-                      ),
-                      const SizedBox(
-                        width: 20,
-                      ),
-                      IconButton(
-                          onPressed: () {
-                            Navigator.pushReplacement(context,
-                                MaterialPageRoute(builder: (context)=> const StudentNewsPageSection2())
-                            );
-                          },
-                          icon: const Icon(
-                            CupertinoIcons.news_solid,
-                            color: Colors.white,
-                            size: 30,
-                          )),
-                      const SizedBox(
-                        width: 20,
-                      ),
-                      IconButton(
-                          onPressed: () {},
-                          icon: const Icon(
-                            Icons.add_business,
-                            color: Colors.black,
-                            size: 30,
-                          ))
-                    ],
-                  ),
-                  const Row(
-                    children: [
-                      SizedBox(
-                        width: 33,
-                      ),
-                      Text('سرا',
-                          style: TextStyle(color: Colors.black, fontSize: 18)),
-                      SizedBox(
-                        width: 40,
-                      ),
-                      Text('حسابا',
-                          style: TextStyle(color: Colors.black, fontSize: 18)),
-                      SizedBox(
-                        width: 30,
-                      ),
-                      Text('کارا',
-                          style: TextStyle(color: Colors.black, fontSize: 18)),
-                      SizedBox(
-                        width: 40,
-                      ),
-                      Text('کلاسا',
-                          style: TextStyle(color: Colors.black, fontSize: 18)),
-                      SizedBox(
-                        width: 40,
-                      ),
-                      Text('خبرا',
-                          style: TextStyle(color: Colors.white, fontSize: 18)),
-                      SizedBox(
-                        width: 40,
-                      ),
-                      Text('تمرینا',
-                          style: TextStyle(color: Colors.black, fontSize: 18)),
-                    ],
-                  )
-                ],
-              ),
-            )
-          ],
-        ),
+                        ))
+                  ],
+                ),
+                const Row(
+                  children: [
+                    SizedBox(
+                      width: 33,
+                    ),
+                    Text('سرا',
+                        style: TextStyle(color: Colors.black, fontSize: 18)),
+                    SizedBox(
+                      width: 40,
+                    ),
+                    Text('حسابا',
+                        style: TextStyle(color: Colors.black, fontSize: 18)),
+                    SizedBox(
+                      width: 30,
+                    ),
+                    Text('کارا',
+                        style: TextStyle(color: Colors.black, fontSize: 18)),
+                    SizedBox(
+                      width: 40,
+                    ),
+                    Text('کلاسا',
+                        style: TextStyle(color: Colors.black, fontSize: 18)),
+                    SizedBox(
+                      width: 40,
+                    ),
+                    Text('خبرا',
+                        style: TextStyle(color: Colors.white, fontSize: 18)),
+                    SizedBox(
+                      width: 40,
+                    ),
+                    Text('تمرینا',
+                        style: TextStyle(color: Colors.black, fontSize: 18)),
+                  ],
+                )
+              ],
+            ),
+          )
+        ],
       ),
     );
   }
 }
-
-
